f357b43ddeb15ddb04e9716d5dbab6d0d9d37f70
asmrajuwan <rajuwan.rj@gmail.com>
2023-07-12 00:27:36 +0600

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+
+
+export default function SetupPage() {
+  return (
+    <div className="p-4">
+      <Modal title="test" description="test desc" isOpen onClose={() => {}}>children
+      </Modal>
+    </div>
+  )
+}
+ 

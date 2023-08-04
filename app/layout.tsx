2d1a2cdd8133ae28cb1222701ec68eb864cb362c
asmrajuwan <rajuwan.rj@gmail.com>
2023-08-05 01:11:56 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ModalProvider } from "@/providers/modal-provider"
 import { ThemeProvider } from "@/providers/theme-provider"
 import { ToastProvider } from "@/providers/toast-provider"
 import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -19,10 +20,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      layout:{logoPlacement: "inside"},
+      baseTheme: dark}}
+    >
      <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <ToastProvider/>
         <ModalProvider />{children}
         </ThemeProvider></body>
